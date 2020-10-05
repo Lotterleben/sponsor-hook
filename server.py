83e25f80086840b9783b6ce0c4c5c62c5e58e890
Lotte Steenbrink <lotte.steenbrink@ferrous-systems.com>
2020-10-05 16:26:06 +0200

    basic sponsor adding on issue action works

--- server.py
+++ server.py
@@ -1,14 +1,43 @@
 #!/usr/bin/env python3
-
+import json
+import requests
 from bottle import route, run, template, request
+
+# Authentication for the user who is adding the sponsor.
+USERNAME = '' # read from config.json
+API_KEY = ''  # read from config.json
 
 # NOTE:
 # - Webhook contenttype must be set to `application/json`
+# - `config.json` must contain github credentials (see `config.json.sample`)
+
+ORG_API_URL = 'https://api.github.com/orgs/congenial-guacamole-org/'
+
+# TODO. also mention username for log readig convenience
+def add_sponsor(invitee_id: int):
+    print('adding sponsor with id', id)
+
+    invitations_endpoint = ORG_API_URL + 'invitations'
+
+    session = requests.Session()
+    session.auth = (USERNAME, API_KEY)
+
+    invitation = {'invitee_id': invitee_id,
+                  'role': 'direct_member'} # TODO: add team ids
+
+    result = session.post(invitations_endpoint, json.dumps(invitation))
+    if result.status_code == 201:
+        print('Successfully added sponsor "%s"' % invitee_id)
+    else:
+        print('Failed to add sponsor "%s"' % invitee_id)
+        print('Response:', result.content)
+
 
 @route('/payload', method=['GET', 'POST'])
 def index():
     print("got request: ")
     print(request)
+
     if request.method == 'POST':
         # TODO https://docs.github.com/en/free-pro-team@latest/developers/webhooks-and-events/securing-your-webhooks
         # TODO Check if the POST request is from github.com
@@ -18,12 +47,26 @@ def index():
         if payload['action'] == 'created':
             # only take action when a new sponsorship is created
 
-            # TODO pull info from payload['sponsorship']['sponsor']['html_url'] instead
-            sender = payload['sender']['html_url'] # todo pick whatever info we need to subscribe that person
-            print("our new sponsor is: ", sender)
+            # TODO pull info from payload['sponsorship']['sponsor']['id'] instead
+            sender_gh_user_id = payload['sender']['id']
+            print("our new sponsor is: ", sender_gh_user_id)
 
-            # TODO take action
+            add_sponsor(sender_gh_user_id)
 
             return 'OK'
 
-run(host='localhost', port=4567)
+    # DELETEME only for debugging
+    elif request.method == 'GET':
+        sender_gh_user_id = 581552 # my own id to prevent spamming anoyne
+        add_sponsor(sender_gh_user_id)
+
+if __name__ == '__main__':
+    with open('config.json', 'r') as config:
+        config_json = json.loads(config.read())
+        USERNAME = config_json['username']
+        API_KEY = config_json['api_key']
+    # TODO add nice error msg if config is missing
+
+    # start server
+    run(host='localhost', port=4567)
+
